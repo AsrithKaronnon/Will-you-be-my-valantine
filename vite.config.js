b7caf03f4c24ebb35af908c405908dbc70d0aa49
Asrith Karonnon <Asrithk@heveahill.com>
2026-02-03 22:36:19 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Will-you-be-my-valantine/',
+  base: './',
   server: {
     host: true,
     proxy: {
